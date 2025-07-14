b9d35ea4056c281fa9a88567f731f1dbea1d3fe8
Philipp0705 <philipp.paessens@smartcondata.de>
2025-07-14 15:06:56 +0200

    Added POST, PUT and DELETE

--- src/app.ts
+++ src/app.ts
@@ -15,8 +15,36 @@ const liste: Artikel[] = [
     {id: 2, name: 'Brot', menge: 1, status: 'erledigt'}
 ]
 
+app.use(express.json())
+
 app.get('/items', (req, res) => {
     res.json(liste)
+})
+
+app.post('/items', (req, res) => {
+    const {menge, name, status} = req.body
+    const neueId = liste.length > 0 ? Math.max(...liste.map(item => item.id)) + 1 : 1
+    const neuerArtikel = {name, menge, status, id: neueId}
+
+    liste.push(neuerArtikel)
+    res.status(201).json(neuerArtikel)
+})
+
+app.put('/items/:id', (req, res) => {
+    const {name, menge, status} = req.body
+    const id = Number(req.params.id)
+    const artikelStelle = liste.findIndex(item => item.id === id)
+    liste[artikelStelle] = {...liste[artikelStelle],name, menge, status}
+
+    res.json(liste[artikelStelle])
+})
+
+app.delete('/items/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const artikelStelle = liste.findIndex(item => item.id === id)
+    liste.splice(artikelStelle, 1)
+    
+    res.json({ nachricht: 'Artikel gelöscht' })
 })
 
 app.listen(port, () => {
