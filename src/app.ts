34bd8fed6d239994fe8948858a9e275ff33a930c
Philipp0705 <philipp.paessens@smartcondata.de>
2025-07-15 12:08:18 +0200

    Added different lists for each user

--- src/app.ts
+++ src/app.ts
@@ -13,75 +13,54 @@ interface Artikel {
     kategorie: string
     fav: "yes" | "no"
     id: number
-    user: string
 }
 
-const liste: Artikel[] = [
-    { id: 1, artikel: 'Milch', menge: 2, status: 'done', kategorie: "Kühl", fav: "no", user: "1" },
-    { id: 2, artikel: 'Brot', menge: 1, status: 'undone', kategorie: "Essen", fav: "yes", user: "2" }
-]
+const liste: Record<string, Artikel[]> = {}
 
 app.use(cors());
 app.use(express.json())
 
-app.get('/items', (req, res) => {
-    return res.json(liste)
+app.get('/users/:user/items', (req, res) => {
+    const user = req.params.user
+    return res.json(liste[user])
 })
 
-app.post('/items', (req, res) => {
-    const { artikel, menge, status, kategorie, fav, user } = req.body
-    const neueId = liste.length > 0 ? Math.max(...liste.map(item => item.id)) + 1 : 1
-    const neuerArtikel = { artikel, menge, status, kategorie, fav, id: neueId, user }
+app.post('/users/:user/items', (req, res) => {
+    const user = req.params.user
+    if (!liste[user]) {
+        liste[user] = []
+    }
+
+    const { artikel, menge, status, kategorie, fav } = req.body
+    const neueId = liste[user].length > 0 ? Math.max(...liste[user].map(item => item.id)) + 1 : 1
+    const neuerArtikel = { artikel, menge, status, kategorie, fav, id: neueId }
     console.log("Neuer Artikel empfangen:", req.body)
-    if (artikel.trim() === "") {
-        return res.status(404).json({ nachricht: 'Bitte einen gültigen Artikel eingeben' })
-    }
-    if (typeof menge !== 'number' || isNaN(menge)) {
-        return res.status(404).json({ nachricht: 'Menge darf nur eine Zahl sein' })
-    }
-    if (menge <= 0) {
-        return res.status(404).json({ nachricht: 'Menge muss größer als 0 sein' })
-    }
-    if (status !== 'undone' && status !== 'done') {
-        return res.status(404).json({ nachricht: 'Bitte gültigen Status eingeben (done / undone' })
-    }
-    liste.push(neuerArtikel)
+    liste[user].push(neuerArtikel)
     return res.status(201).json(neuerArtikel)
 })
 
-app.put('/items/:id', (req, res) => {
-    const { artikel, menge, status, kategorie, fav, user } = req.body
+app.put('/users/:user/items/:id', (req, res) => {
+    const user = req.params.user
     const id = Number(req.params.id)
+    const { artikel, menge, status, kategorie, fav } = req.body
     console.log("Artikel geändert:", req.body)
-    if (artikel.trim() === "") {
-        return res.status(404).json({ nachricht: 'Bitte einen gültigen Artikel eingeben' })
-    }
-    if (typeof menge !== 'number' || isNaN(menge)) {
-        return res.status(404).json({ nachricht: 'Menge darf nur eine Zahl sein' })
-    }
-    if (menge <= 0) {
-        return res.status(404).json({ nachricht: 'Menge muss größer als 0 sein' })
-    }
-    if (status !== 'undone' && status !== 'done') {
-        return res.status(404).json({ nachricht: 'Bitte gültigen Status eingeben (offen / erledigt' })
-    }
-
-    const artikelStelle = liste.findIndex(item => item.id === id)
+    const artikelStelle = liste[user].findIndex(item => item.id === id)
     if (artikelStelle === -1) {
         return res.status(404).json({ nachricht: 'Artikel wurde nicht gefunden!' })
     }
-    liste[artikelStelle] = { ...liste[artikelStelle], artikel, menge, status, kategorie, fav, user }
-    return res.json(liste[artikelStelle])
+    liste[user][artikelStelle] = { ...liste[user][artikelStelle], artikel, menge, status, kategorie, fav }
+    return res.json(liste[user][artikelStelle])
 })
 
-app.delete('/items/:id', (req, res) => {
+app.delete('/users/:user/items/:id', (req, res) => {
+    const user = req.params.user
     const id = Number(req.params.id)
-    const artikelStelle = liste.findIndex(item => item.id === id)
+    const artikelStelle = liste[user].findIndex(item => item.id === id)
     console.log("Artikel gelöscht:", req.body)
     if (artikelStelle === -1) {
         return res.status(404).json({ nachricht: 'Artikel wurde nicht gefunden!' })
     }
-    liste.splice(artikelStelle, 1)
+    liste[user].splice(artikelStelle, 1)
 
     return res.json({ nachricht: 'Artikel gelöscht' })
 })
